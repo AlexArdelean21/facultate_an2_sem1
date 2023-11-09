fbc5053793017f22a1fa905aef01e6b6fd0d1b91
Alex Ardelean <ardeleanalexandru22@stud.ase.ro>
2023-11-09 10:50:44 +0200

    Adds seminar 7

---
+++ seminar 7/main.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include<cassert>
+using namespace std;
+
+class Fractie{
+    int m_numarator{ 0 };
+    int m_numitor{ 1 }; 
+    static int count;
+public:
+    
+    static void setCount(static int _count) {
+            Fractie::count = _count;
+    }
+
+    static int getCount() { return Fractie::count; }
+
+    // Default constructor
+    Fractie(int numarator = 0, int numitor = 1)
+        : m_numarator(numarator), m_numitor(numitor)
+    {
+        assert(numitor != 0);
+        Fractie::count++;
+    }
+
+    // Copy constructor
+    Fractie(const Fractie& copy)
+        : m_numarator{ copy.m_numarator }, m_numitor{ copy.m_numitor }
+    {
+        // nu mai este nevoie de verificarea numitorului pentru ca operatiile se fac asupra a doua obiecte existente - au fost verificate in cadrul constructorului
+        cout << endl << "S-a apelat constructorul de copiere.\n";
+        Fractie::count++;
+    }
+
+    // Supraincarcarea operatorului de atribuire:  =
+   // Fractie& operator= (const Fractie& fractie);
+    //void operator= (const Fractie& fractie);
+
+   // Fractie& operator= (const Fractie& fractie) = delete; // nu se permite efectuarea copiilor prin asignare!
+};
+
+int Fractie::count = 0;
+// O alta implementare => void??
+
+//void Fractie::operator= (const Fractie& fractie)
+//{
+//    // se evita autoasignarea
+//    if (this == &fractie)
+//        return;
+//
+//    m_numarator = fractie.m_numarator;
+//    m_numitor = fractie.m_numitor;
+//
+//
+//    return;
+//}
+
+
+// O implementare mai buna - corecta => discutie
+
+Fractie& Fractie::operator= (const Fractie& fractie)
+{
+    // se evita autoasignarea
+    if (this == &fractie)
+        return *this;
+
+    m_numarator = fractie.m_numarator;
+    m_numitor = fractie.m_numitor;
+
+
+    return *this;
+}
+
+
+
+
+int main()
+{
+ //   Fractie ff(1, 2);
+ //   Fractie ff1;
+ //   ff1 = ff;
+
+ //   Fractie ff2 = ff;
+
+   
+    Fractie _5p3{ 5, 3 };
+    Fractie f = _5p3;
+    Fractie ff;      //cine se apeleaza?
+    ff = _5p3; //cine se apeleaza?
+
+    Fractie f1{ 5, 3 };
+    Fractie f2{ 7, 2 };
+    Fractie f3{ 9, 5 };
+
+    f1 = f2 = f3; //asignare inlantuita
+    //f1 = f2;// = f3; //asignare inlantuita
+
+    //f1 = f1; // autoasignare
+
+    Fractie::setCount(50);
+    cout << endl << Fractie::getCount() << endl;
+    return 0;
+}
